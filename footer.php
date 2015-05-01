cd2e8397d7484f169417b428cb92134c378a7434
Alex Chang <alexc@mumms.com>
2015-05-01 13:52:15 -0500

    added padding to the footer components via css

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
           <div class="col-lg-12">
              <div class="textwidget">You must be 18 years of age or 21 in some areas to view the Crazy Horse Port Allen web site. This site contains adult material not suitable for minors. Viewing this site if you are under age is prohibited by federal, state and local laws and is subject to prosecution by the applicable authorities. By clicking above you agree to being familiar with the laws of your community and certify that you are of age to legally view adult material. Furthermore you agree that you do not find this type of material offensive and you release and discharge all involved in the production and maintenance of this site from any and all liabilities.</div>
           </div>
-          
+         
           <div class="footer-logos">
                 <a href="http://kirkmgmt.com/" title="Kirkendoll Management, LLC." target="_blank"><img src="/img/logos/km_management.png"  alt="Kirkendoll Management, LLC."  title="Kirkendoll Management, LLC." /></a>
                 <a href="http://penthouseclubneworleans.com/" title="The Penthouse Club &#8211; New Orleans" target="_blank"><img src="/img/logos/ph-new-orleans.png"  alt="The Penthouse Club &#8211; New Orleans"  title="The Penthouse Club &#8211; New Orleans" /></a>
@@ -12,7 +12,7 @@
                 <a href="http://crazyhorsecabaret.com/" title="Crazy Horse Cabaret &#8211; Port Allen" target="_blank"><img src="/img/logos/chc-port-allen.png"  alt="Crazy Horse Cabaret &#8211; Port Allen"  title="Crazy Horse Cabaret &#8211; Port Allen" /></a>
                 <a href="http://crazyhorsecabaret2.com/" title="Crazy Horse Cabaret &#8211; Gonzales" target="_blank"><img src="/img/logos/chc-gonzales.png"  alt="Crazy Horse Cabaret &#8211; Gonzales"  title="Crazy Horse Cabaret &#8211; Gonzales" /></a>
           </div>
-          <p/>
+         
           <div class="col-lg-12">
             <p class="copyright">&copy; 2015 Kirkendoll Management. All Rights Reserved.</p>
           </div>
