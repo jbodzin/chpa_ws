b4e82d1fb46e7fcfdca9c74bcf5fd8258d7e7ff7
Alex Chang <alexc@mumms.com>
2015-05-01 12:34:11 -0500

    added transparent logos for footer and centered

--- footer.php
+++ footer.php
@@ -6,11 +6,11 @@
           </div>
           
           <div class="footer-logos">
-                <a href="http://kirkmgmt.com/" title="Kirkendoll Management, LLC." target="_blank"><img src="http://www.penthousebatonrouge.com/wp-content/themes/penthouse_batonrouge/img/km_management.jpg"  alt="Kirkendoll Management, LLC."  title="Kirkendoll Management, LLC." /></a>
-                <a href="http://penthouseclubneworleans.com/" title="The Penthouse Club &#8211; New Orleans" target="_blank"><img src="http://www.penthousebatonrouge.com/wp-content/themes/penthouse_batonrouge/img/ph-new-orleans.jpg"  alt="The Penthouse Club &#8211; New Orleans"  title="The Penthouse Club &#8211; New Orleans" /></a>
-                <a href="http://penthousebatonrouge.com/" title="The Penthouse Club &#8211; Baton Rouge" target="_blank"><img src="http://www.penthousebatonrouge.com/wp-content/themes/penthouse_batonrouge/img/ph-baton-rouge.jpg"  alt="The Penthouse Club &#8211; Baton Rouge"  title="The Penthouse Club &#8211; Baton Rouge" /></a>
-                <a href="http://crazyhorsecabaret.com/" title="Crazy Horse Cabaret &#8211; Port Allen" target="_blank"><img src="http://www.penthousebatonrouge.com/img/chc-port-allen.jpg"  alt="Crazy Horse Cabaret &#8211; Port Allen"  title="Crazy Horse Cabaret &#8211; Port Allen" /></a>
-                <a href="http://crazyhorsecabaret2.com/" title="Crazy Horse Cabaret &#8211; Gonzales" target="_blank"><img src="http://www.penthousebatonrouge.com/img/chc-gonzales.jpg"  alt="Crazy Horse Cabaret &#8211; Gonzales"  title="Crazy Horse Cabaret &#8211; Gonzales" /></a>
+                <a href="http://kirkmgmt.com/" title="Kirkendoll Management, LLC." target="_blank"><img src="/img/logos/km_management.png"  alt="Kirkendoll Management, LLC."  title="Kirkendoll Management, LLC." /></a>
+                <a href="http://penthouseclubneworleans.com/" title="The Penthouse Club &#8211; New Orleans" target="_blank"><img src="/img/logos/ph-new-orleans.png"  alt="The Penthouse Club &#8211; New Orleans"  title="The Penthouse Club &#8211; New Orleans" /></a>
+                <a href="http://penthousebatonrouge.com/" title="The Penthouse Club &#8211; Baton Rouge" target="_blank"><img src="/img/logos/ph-baton-rouge.png"  alt="The Penthouse Club &#8211; Baton Rouge"  title="The Penthouse Club &#8211; Baton Rouge" /></a>
+                <a href="http://crazyhorsecabaret.com/" title="Crazy Horse Cabaret &#8211; Port Allen" target="_blank"><img src="/img/logos/chc-port-allen.png"  alt="Crazy Horse Cabaret &#8211; Port Allen"  title="Crazy Horse Cabaret &#8211; Port Allen" /></a>
+                <a href="http://crazyhorsecabaret2.com/" title="Crazy Horse Cabaret &#8211; Gonzales" target="_blank"><img src="/img/logos/chc-gonzales.png"  alt="Crazy Horse Cabaret &#8211; Gonzales"  title="Crazy Horse Cabaret &#8211; Gonzales" /></a>
           </div>
           <p/>
           <div class="col-lg-12">
